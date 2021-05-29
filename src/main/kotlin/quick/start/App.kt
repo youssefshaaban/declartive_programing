06c0f98dda8b4996ea949a92434e4f913f8c979e
DESKTOP-TGK889M\youse <Dev@smartzone-it.com>
2021-05-29 20:50:02 +0200

    session1

--- src/main/kotlin/quick/start/App.kt
+++ src/main/kotlin/quick/start/App.kt
@@ -8,6 +8,20 @@ class App {
         get() = "Hello world."
 }
 
-fun main(args: Array<String>) {
-    println(App().greeting)
+fun main() {
+    val listOfDouble=listOf<Double>(7.0,4.0,5.0,6.0,3.0,8.0,10.0)
+
+    // imprative
+    //listOfDouble.forEach {x-> println(substractTen(square(addOne(x)))) }
+    listOfDouble.map { x->addOne(x) }.map { x->square(x) }.filter { x->x<70 }
+    .sorted()
+    .take(2)
+    .map { x->substractTen(x) }.toList().forEach{
+        x->println(x)
+    }
+    
 }
+
+fun addOne(x:Double): Double = x + 1
+fun square(x:Double): Double = x * x
+fun substractTen(x:Double): Double = x - 10
